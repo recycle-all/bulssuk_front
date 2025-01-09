6b4f7d4aaf83eff4cfa812367c0cef9c67b9c7f4
“dongkyu” <kjuvcz95@gamil.com>
2025-01-09 11:13:03 +0900

    shopping_page_imgSize

--- lib/shopping/shopping_page.dart
+++ lib/shopping/shopping_page.dart
@@ -276,8 +276,8 @@ class _ShoppingPageState extends State<ShoppingPage> {
                   children: [
                     // 상품 이미지
                     Container(
-                      width: 160,
-                      height: 160,
+                      width: 150,
+                      height: 150,
                       decoration: BoxDecoration(
                         color: Colors.grey[200],
                         borderRadius: BorderRadius.circular(12.0),
